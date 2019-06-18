9f11ac8cf2f0913f65b517102797df25be4b7472
Jake <jacobstefan92@gmail.com>
2019-06-18 11:08:07 -0400

    proof read list doc

--- resources/views/documentation/components/list.blade.php
+++ resources/views/documentation/components/list.blade.php
@@ -23,8 +23,10 @@
 <article class="section-scroll" id="usage">
     <h2 class="b-b-light hash">Usage</h2>
     <p class="secondary-lead">
-        A list is a way of portraying similar data in an orderly fashion. List items should be wrapped in a <code class="inline">&lt;ul&gt;</code> element with a <code class="inline">list</code> class attached to it.
-        <br>Each <code class="inline">&lt;li&gt;</code> or <code class="inline">&lt;a&gt;</code> tag within the list should have a <code class="inline">list-item</code> class attached to it, see below:
+        A list is a way of portraying similar data in an orderly fashion. List items should be wrapped in a <code
+            class="inline">&lt;ul&gt;</code> element with a <code class="inline">list</code> class attached to it.
+        <br>Each <code class="inline">&lt;li&gt;</code> or <code class="inline">&lt;a&gt;</code> tag within the list
+        should have a <code class="inline">list-item</code> class attached to it, see below:
     </p>
     <ul class="list">
         <li class="list-item">List item</li>
@@ -33,10 +35,11 @@
         <li class="list-item">List item</li>
         <li class="list-item">List item</li>
     </ul>
-    <pre class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
+    <pre
+        class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
     &lt;li class=&quot;list-item&quot;&gt;List item&lt;/li&gt;
     &lt;li class=&quot;list-item&quot;&gt;List item&lt;/li&gt;
-    &lt;li claszs=&quot;list-item&quot;&gt;List item&lt;/li&gt;
+    &lt;li class=&quot;list-item&quot;&gt;List item&lt;/li&gt;
     &lt;li class=&quot;list-item&quot;&gt;List item&lt;/li&gt;
     &lt;li class=&quot;list-item&quot;&gt;List item&lt;/li&gt;
 &lt;/ul&gt;</code><img class="copy-to-clipboard"src="/assets/icons/copy.svg" alt="Copy icon"><img class="copy-tick"src="/assets/icons/checked.svg" alt="Success icon"></pre>
@@ -46,7 +49,8 @@
 <article class="section-scroll" id="active">
     <h2 class="b-b-light hash">Active</h2>
     <p class="secondary-lead">
-        In order to display a colored background on one of the list items, simply add the <code class="inline">active</code> class
+        In order to display a colored background on one of the list items, simply add the <code
+            class="inline">active</code> class
     </p>
     <ul class="list">
         <li class="list-item active">List item</li>
@@ -55,7 +59,8 @@
         <li class="list-item">List item</li>
         <li class="list-item">List item</li>
     </ul>
-    <pre class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
+    <pre
+        class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
     &lt;li class=&quot;list-item active&quot;&gt;List item&lt;/li&gt;
     &lt;li class=&quot;list-item&quot;&gt;List item&lt;/li&gt;
     &lt;li class=&quot;list-item&quot;&gt;List item&lt;/li&gt;
@@ -68,7 +73,8 @@
 <article class="section-scroll" id="disabled">
     <h2 class="b-b-light hash">Disabled</h2>
     <p class="secondary-lead">
-        To make a list item disabled, add the <code class="inline">disabled</code> class, which will slightly tone the color of the text down.
+        To make a list item disabled, add the <code class="inline">disabled</code> class, which will slightly mute the
+        color of the text.
     </p>
     <ul class="list">
         <li class="list-item disabled">List item</li>
@@ -77,7 +83,8 @@
         <li class="list-item">List item</li>
         <li class="list-item">List item</li>
     </ul>
-    <pre class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
+    <pre
+        class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
     &lt;li class=&quot;list-item disabled&quot;&gt;List item&lt;/li&gt;
     &lt;li class=&quot;list-item&quot;&gt;List item&lt;/li&gt;
     &lt;li class=&quot;list-item&quot;&gt;List item&lt;/li&gt;
@@ -90,7 +97,8 @@
 <article class="section-scroll" id="link">
     <h2 class="b-b-light hash">Link</h2>
     <p class="secondary-lead">
-        Instead of using <code class="inline">&lt;li&gt;</code> tags you can also use <code class="inline">a</code> links. Which will add a <code class="inline">display: block;</code> property to stack them vertically.
+        Instead of using <code class="inline">&lt;li&gt;</code> tags you can also use <code class="inline">a</code>
+        links. Which will add a <code class="inline">display: block;</code> property to stack them vertically.
     </p>
     <ul class="list">
         <a href="#!" class="list-item">List item</a>
@@ -99,7 +107,8 @@
         <a href="#!" class="list-item">List item</a>
         <a href="#!" class="list-item">List item</a>
     </ul>
-    <pre class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
+    <pre
+        class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
     &lt;a href=&quot;#!&quot; class=&quot;list-item&quot;&gt;List item&lt;/a&gt;
     &lt;a href=&quot;#!&quot; class=&quot;list-item active&quot;&gt;List item&lt;/a&gt;
     &lt;a href=&quot;#!&quot; class=&quot;list-item&quot;&gt;List item&lt;/a&gt;
@@ -112,13 +121,15 @@
 <article class="section-scroll" id="icon">
     <h2 class="b-b-light hash">Icon</h2>
     <p class="secondary-lead">
-        Icons can also be used in conjunction with a <code class="inline">list-item</code> element. Simply add the <code class="inline">list-icon</code> class, which will add the <code class="inline">display: flex;</code> property.
+        Icons can also be used in conjunction with a <code class="inline">list-item</code> element. Simply add the <code
+            class="inline">list-icon</code> class, which will add the <code class="inline">display: flex;</code>
+        property.
     </p>
     <ul class="list">
         <a href="#!" class="list-item list-icon">
             List item
             <i class="fas fa-caret-right fa-lg"></i>
-        </a>                        
+        </a>
         <a href="#!" class="list-item list-icon active">
             List item
             <i class="fas fa-caret-right fa-lg"></i>
@@ -126,7 +137,7 @@
         <a href="#!" class="list-item list-icon">
             List item
             <i class="fas fa-caret-right fa-lg"></i>
-        </a>                        
+        </a>
         <a href="#!" class="list-item list-icon">
             List item
             <i class="fas fa-caret-right fa-lg"></i>
@@ -134,9 +145,10 @@
         <a href="#!" class="list-item list-icon">
             List item
             <i class="fas fa-caret-right fa-lg"></i>
-        </a>                        
+        </a>
     </ul>
-    <pre class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
+    <pre
+        class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
     &lt;a href=&quot;#!&quot; class=&quot;list-item list-icon&quot;&gt;
         List item
         &lt;i class=&quot;fas fa-caret-right fa-lg&quot;&gt;&lt;/i&gt;
@@ -163,14 +175,16 @@
 <article class="section-scroll" id="badge">
     <h2 class="b-b-light hash">Badge</h2>
     <p class="secondary-lead">
-        Use the mesh badges to create notifications within the list. Use the <code class="inline">list-icon</code> class and add the badge.
-        <br><strong>Note:</strong> you may need to add our color utility classes to achieve the right appearance, see below:
+        Use the mesh badges to create notifications within the list. Use the <code class="inline">list-icon</code> class
+        and add the badge.
+        <br><strong>Note:</strong> you may need to add our color utility classes to achieve the right appearance, see
+        below:
     </p>
     <ul class="list">
         <a href="#!" class="list-item list-icon">
             List item
             <div class="badge badge-primary badge-rounded">2</div>
-        </a>                        
+        </a>
         <a href="#!" class="list-item list-icon active">
             List item
             <div class="badge badge-primary badge-rounded bg-white c-primary">4</div>
@@ -178,9 +192,10 @@
         <a href="#!" class="list-item list-icon">
             List item
             <div class="badge badge-primary badge-rounded">14</div>
-        </a>                        
+        </a>
     </ul>
-    <pre class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
+    <pre
+        class="highlight"><code class="html">&lt;ul class=&quot;list&quot;&gt;
     &lt;a href=&quot;#!&quot; class=&quot;list-item list-icon&quot;&gt;
         List item
         &lt;div class=&quot;badge badge-primary badge-rounded&quot;&gt;2&lt;/div&gt;
@@ -207,27 +222,37 @@
                 <h3 class="mb-0">List item heading</h3>
                 <div class="badge badge-primary">Posted 2 days ago</div>
             </div>
-            <p>Kogi single-origin coffee farm-to-table letterpress keffiyeh heirloom. Helvetica cold-pressed readymade meggings, echo park retro sartorial. +1 wolf subway tile, quinoa heirloom live-edge ethical letterpress blog kale chips vape pok pok butcher. Everyday carry kombucha gochujang VHS, poke man braid copper mug asymmetrical cronut. Man braid viral fashion axe thundercats try-hard church-key literally quinoa.</p>
+            <p>Kogi single-origin coffee farm-to-table letterpress keffiyeh heirloom. Helvetica cold-pressed readymade
+                meggings, echo park retro sartorial. +1 wolf subway tile, quinoa heirloom live-edge ethical letterpress
+                blog kale chips vape pok pok butcher. Everyday carry kombucha gochujang VHS, poke man braid copper mug
+                asymmetrical cronut. Man braid viral fashion axe thundercats try-hard church-key literally quinoa.</p>
             <h6>Posted by Tiger Woods</h6>
-        </a>                        
+        </a>
         <a href="#!" class="list-item active">
             <div class="d-flex justify-content-between align-items-center my-2">
                 <h3 class="mb-0">List item heading</h3>
                 <div class="badge badge-white">Posted 5 days ago</div>
             </div>
-            <p>Kogi single-origin coffee farm-to-table letterpress keffiyeh heirloom. Helvetica cold-pressed readymade meggings, echo park retro sartorial. +1 wolf subway tile, quinoa heirloom live-edge ethical letterpress blog kale chips vape pok pok butcher. Everyday carry kombucha gochujang VHS, poke man braid copper mug asymmetrical cronut. Man braid viral fashion axe thundercats try-hard church-key literally quinoa.</p>
+            <p>Kogi single-origin coffee farm-to-table letterpress keffiyeh heirloom. Helvetica cold-pressed readymade
+                meggings, echo park retro sartorial. +1 wolf subway tile, quinoa heirloom live-edge ethical letterpress
+                blog kale chips vape pok pok butcher. Everyday carry kombucha gochujang VHS, poke man braid copper mug
+                asymmetrical cronut. Man braid viral fashion axe thundercats try-hard church-key literally quinoa.</p>
             <h6>Posted by Gordon Ramsey</h6>
-        </a> 
+        </a>
         <a href="#!" class="list-item w-100 c-dark">
             <div class="d-flex justify-content-between align-items-center my-2">
                 <h3 class="mb-0">List item heading</h3>
                 <div class="badge badge-primary">Posted 10 days ago</div>
             </div>
-            <p>Kogi single-origin coffee farm-to-table letterpress keffiyeh heirloom. Helvetica cold-pressed readymade meggings, echo park retro sartorial. +1 wolf subway tile, quinoa heirloom live-edge ethical letterpress blog kale chips vape pok pok butcher. Everyday carry kombucha gochujang VHS, poke man braid copper mug asymmetrical cronut. Man braid viral fashion axe thundercats try-hard church-key literally quinoa.</p>
+            <p>Kogi single-origin coffee farm-to-table letterpress keffiyeh heirloom. Helvetica cold-pressed readymade
+                meggings, echo park retro sartorial. +1 wolf subway tile, quinoa heirloom live-edge ethical letterpress
+                blog kale chips vape pok pok butcher. Everyday carry kombucha gochujang VHS, poke man braid copper mug
+                asymmetrical cronut. Man braid viral fashion axe thundercats try-hard church-key literally quinoa.</p>
             <h6>Posted by Above & Beyond</h6>
-        </a>                       
+        </a>
     </ul>
-    <pre class="highlight"><code class="html">&lt;ul class=&quot;list w-100 w-tab-60&quot;&gt;
+    <pre
+        class="highlight"><code class="html">&lt;ul class=&quot;list w-100 w-tab-60&quot;&gt;
     &lt;a href=&quot;#!&quot; class=&quot;list-item w-100 c-dark&quot;&gt;
         &lt;div class=&quot;d-flex justify-content-between align-items-center my-2&quot;&gt;
             &lt;h3 class=&quot;mb-0&quot;&gt;List item heading&lt;/h3&gt;
@@ -264,37 +289,47 @@
     </p>
     <div class="table-scroll">
         <table class="table">
-                <thead>
-                    <tr>
-                        <th>Variable</th>
-                        <th class="w-15">Value</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td style="vertical-align: middle;"><pre class="variables highlight"><code class="scss">$list-background-color: #fff;</code></pre></td>
-                        <td>Color</td>
-                        <td>The default background color of the list component.</td>
-                    </tr>
-                    <tr>
-                        <td style="vertical-align: middle;"><pre class="variables highlight"><code class="scss">$list-border-color: #ddd;</code></pre></td>
-                        <td>Color</td>
-                        <td>The border color of the list component.</td>
-                    </tr>
-                    <tr>
-                        <td><pre class="variables highlight"><code class="scss">$list-active-color: color('primary');</code></pre></td>
-                        <td>Color</td>
-                        <td>The active (user selected) color of the list component, the default takes the primary color from the color palette.</td>
-                    </tr>
-                    <tr>
-                        <td><pre class="variables highlight"><code class="scss">$list-disabled-color: #999;</code></pre></td>
-                        <td>Color</td>
-                        <td>The disabled color of a list item.</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+            <thead>
+                <tr>
+                    <th>Variable</th>
+                    <th class="w-15">Value</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="vertical-align: middle;">
+                        <pre class="variables highlight"><code class="scss">$list-background-color: #fff;</code></pre>
+                    </td>
+                    <td>Color</td>
+                    <td>The default background color of the list component.</td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: middle;">
+                        <pre class="variables highlight"><code class="scss">$list-border-color: #ddd;</code></pre>
+                    </td>
+                    <td>Color</td>
+                    <td>The border color of the list component.</td>
+                </tr>
+                <tr>
+                    <td>
+                        <pre
+                            class="variables highlight"><code class="scss">$list-active-color: color('primary');</code></pre>
+                    </td>
+                    <td>Color</td>
+                    <td>The active (user selected) color of the list component, the default takes the primary color from
+                        the color palette.</td>
+                </tr>
+                <tr>
+                    <td>
+                        <pre class="variables highlight"><code class="scss">$list-disabled-color: #999;</code></pre>
+                    </td>
+                    <td>Color</td>
+                    <td>The disabled color of a list item.</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </article>
 
 @endsection
